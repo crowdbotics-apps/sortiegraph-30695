6105d64c536028cb577a470f93c034ae0275bd08
Crowdbotics <team@crowdbotics.com>
2021-09-20 07:52:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sortiegraph_30695Identifier",
+  url: "https://sortiegraph_30695Identifier.botics.co",
+  api: "https://sortiegraph_30695Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
